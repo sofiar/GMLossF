72bdb30497c763f73b2c58e5666ddbfbc27aa0f4
sofiar <sofi.ruizsuarez@gmail.com>
2024-04-13 09:46:32 -0400

    tidy the file by removing old funcions and adding some new versions

--- Main_scripts/Extrafunctions.R
+++ Main_scripts/Extrafunctions.R
@@ -2,137 +2,192 @@
 ############################ Extra functions ###################################
 ################################################################################
 
-library(Rfast)
-library(foreach)
-library(future.apply)
-library(doParallel)
-library(future.batchtools)
-library(MASS)
-#library(Rcpp)
-#library(RcppArmadillo)
-#library(RcppEigen)
-library(lamW)
+################################################################################
+lambertW_expArg = function(x) {
 
-#sourceCpp("optimfunctions.cpp")
-#subfun2_bis_wrap(Ns = c(10, 20, 30), M = 5000, theta1 = 1.5, theta2 = 1, b = -0.8)
-#subfun2(Ns = c(10, 20, 30), M = 5000, theta1 = 1.5, theta2 = 1, b = -0.8)
+  ctrl = x <= 709
+  res = double(length = length(x))
 
+  if (sum(ctrl) > 0) res[ctrl] = lamW::lambertW0(exp(x[ctrl]))
+  if (sum(!ctrl) > 0) {
+    res[!ctrl] = x[!ctrl] - log(x[!ctrl]) + log(x[!ctrl]) / x[!ctrl] +
+      (log(x[!ctrl]) * (log(x[!ctrl]) - 2)) / (2 * x[!ctrl]^2) +
+      log(x[!ctrl]) * (2 * log(x[!ctrl])^2 - 9 * log(x[!ctrl]) + 6) /
+        (6 * x[!ctrl]^3) + log(x[!ctrl]) *
+      (
+       -12 + 36 * log(x[!ctrl]) - 22 * log(x[!ctrl])^2 +
+         3 * log(x[!ctrl])^3) / (12 * x[!ctrl]^4
+    )
+  }
 
-approx.CL2=function(Nobs,theta1,theta2,b,M,d=NULL)# d<length(Nobs)
-{
-curr=0
-Times=length(Nobs)
-mu=rep(theta1, 2)
-if(is.null(d))
-{d=Times}
-if(d>Times)
-{cat('d must be less than the length of the time series')}
-for (t1 in 1:(Times-1)) {
-limit=min(t1+d,Times)    
-for (t2 in (t1+1):limit) {
-B = (1+b)^(abs(outer(c(1,abs(t2-t1+1)),c(1,abs(t2-t1+1)) , "-"))) # get B matrix
-cov_matrix=theta2*B
-N.sample <- exp(mvrnorm(M, mu = mu, Sigma = cov_matrix))
-logval = colSums(dpois(Nobs[c(t1,t2)],t(N.sample),log=TRUE))
-curr = curr-log(M) + log( sum(exp(logval)))
-}    
-}
-return(curr)
-}
-
-
-get.l=function(theta1t,theta2t,theta3t,Nobs,Nt) 
-{
-abs=to_abs(theta1t,theta2t,theta3t)
-nreps=dim(Nobs)[2]
-TT=dim(Nobs)[1]
-out=0
-for(j in 1:nreps)
-{
-N1obs=Nobs[,j]
-N1t=Nt[,j]
-
-z_hat=numeric(TT)
-vars=numeric(TT)
-z_hat[1]=theta2*N1obs[1]+theta1-lambertW0(theta2*exp(theta2*N1obs[1]+theta1))
-vars[1]=theta2/(1+theta2*exp(-z_hat[1]))
-
-for(n in 2:TT)
-{
- z_hat[n]=abs[['sigma2']]*N1obs[n]+abs[['a']]+(1+abs[['b']])*z_hat[n-1]-lambertW0(theta2*exp(theta2*N1obs[n]+(abs[['a']]+(a+abs[['b']]*z_hat[n-1]))))
- vars[n]=abs[['sigma2']]/(1+abs[['sigma2']]*exp(-z_hat[n]))
-}
-out=out+sum(dnorm(N1t,mean=z_hat,sd=sqrt(vars),log=TRUE))
+  return(res)
 
 }
-return(out)
+################################################################################
+
+
+
+################################################################################
+GompPois_simulate = function(T, theta1, theta2, b) {
+
+  # This function simulates Gompertz model by transformed parameters
+
+  # T is the length of the chain
+
+  res = double(T)
+  a = -b * theta1
+  sigmaSq = -b * (2 + b) * theta2
+
+  res[1] = theta1 + sqrt(theta2) * rnorm(1)
+  for (t in 2:T) {
+    res[t] = a + (1 + b) * res[t - 1] + sqrt(sigmaSq) * rnorm(1)
+  }
+
+  res = rpois(T, lambda = exp(res))
+
+  return(res)
+
 }
+################################################################################
 
 
-lambertW_expArg = function(x)
-{
-if(x<=709)
-{
-out=lamW::lambertW0(exp(x))
-}else{
-  out=x - log(x) + log(x)/x + (log(x)*(log(x)-2))/(2*x^2) +
-    log(x)*(2*log(x)^2-9*log(x)+6)/(6*x^3) +
-    log(x)*(-12+36*log(x)-22*log(x)^2+3*log(x)^3)/(12*x^4)
-}
-return(out)
-}
 
-log_targetPoissonGauss = function(x,n,tauSq,mu){
-  -exp(x) + x*n - 0.5/tauSq * (x-mu)^2
-}
+################################################################################
+Simulate_Gompertz=function(TT,a,b,sigma2){
+  
+  # This function simulates Gompertz model by the original paramters 
 
-log_proposalGauss = function(x,tauSq,xi){
-  - 0.5/tauSq * (x-xi)^2
-}
+  theta1=-a/b
+  theta2=-sigma2/(b*(b+2))
 
+  Nt=numeric(TT)
+  Nt[1]=exp(rnorm(1,theta1,sqrt(theta2)))
+  Es=rnorm(TT,0,sqrt(sigma2))
+  
+  for(t in 2:TT){
+    Nt[t]=Nt[t-1]*exp(a+b*log(Nt[t-1])+Es[(t-1)])  
+  }
 
-Simulate_Gompertz=function(TT,a,b,sigma2)
-{
-theta1=-a/b
-theta2=-sigma2/(b*(b+2))
+  Nt.obs=numeric(TT)
 
-# B = (1+b)^(abs(outer(1:TT, 1:TT, "-"))) # get B matrix
-# Z=theta1+sqrt(theta2)*t(chol(B))%*%rnorm(TT)
-# Nt.obs = rpois(TT, exp(Z)) 
-
-Nt=numeric(TT)
-Nt[1]=exp(rnorm(1,theta1,sqrt(theta2)))
-Es=rnorm(TT,0,sqrt(sigma2))
-for(t in 2:TT)
-{
-Nt[t]=Nt[t-1]*exp(a+b*log(Nt[t-1])+Es[(t-1)])  
-}
-
-Nt.obs=numeric(TT)
-
-for(t in 1:TT)
-{Nt.obs[t]=rpois(1,lambda=Nt[t])}
+  for(t in 1:TT){
+    Nt.obs[t]=rpois(1,lambda=Nt[t])
+  }
 
 return(Nt.obs)
 
 }
+################################################################################
 
 
-##### Simulation Gompertz with log normal error
 
-Simulate_Gompertz_ln = function(TT,a,b,sigma2,tau2)
-{
+################################################################################
+GompPois_simulate_ln = function(TT,a,b,sigma2,tau2) {
+  
+  # Simulation Gompertz with log normal error 
+
   Y = numeric(TT)
   mu = -a/b
   var = sigma2/(1-((1+b)^2))+tau2
   Y[1] = rnorm(1,mu,sqrt(var))
-  for(i in 2:length(Y))
-  {
-  mu = a+(1+b)*(mu+(var-tau2)/var*(Y[i-1]-mu))
-  var = (1+b)^2*(var-tau2)/var*tau2+sigma2+tau2
-  Y[i] = rnorm(1,mu,sqrt(var))
+  
+  for(i in 2:length(Y)){
+    mu = a+(1+b)*(mu+(var-tau2)/var*(Y[i-1]-mu))
+    var = (1+b)^2*(var-tau2)/var*tau2+sigma2+tau2
+    Y[i] = rnorm(1,mu,sqrt(var))
   } 
-return(exp(Y))
-}
 
-## comment to try pull request
+  return(exp(Y))
+
+}
+################################################################################
+
+
+
+################################################################################
+GompPois_MoM = function(Nstar) {
+
+  # Gompertz Model with Poisson Sampling Error: Method of Moments
+  #
+  # the parameters of the model are chosen in order to match the empirical mean,
+  #  variance and autocovariance at lag 1.
+
+  t1 = mean(Nstar)
+  t2prime = var(Nstar) - t1
+  t3 = acf(Nstar, lag.max = 1, plot = FALSE, type = "covariance")$acf[2]
+
+  theta2hat = log(1 + t2prime / t1^2)
+  theta1hat = log(t1) - 0.5 * theta2hat
+  theta3hat = log(1 + t3 * exp(-2 * theta1hat - theta2hat))
+
+  b =  theta3hat / theta2hat - 1
+  a = -theta1hat * b
+  sigmaSq = -theta2hat * b * (2 + b)
+
+  return(list(
+    theta1 = theta1hat,
+    theta2 = theta2hat,
+    b = b,
+    a = a,
+    sigmaSq = sigmaSq
+  ))
+
+}
+################################################################################
+
+
+
+################################################################################
+# old versio of approx.CL2
+GompPois_composite_likelihood=function(Nstar,theta1,theta2,b,nsim,
+                                      d=NULL){
+  
+  # Computes the approximate composite likelihood considering all posible   
+  # combinations of points with d distance
+ 
+  # d<length(Nstar)
+
+  curr=0
+  Times=length(Nstar)
+  mu=rep(theta1, 2)
+
+  if(is.null(d)){d=Times}
+  if(d>Times){cat('d must be less than the length of the time series')}
+  
+  for (t1 in 1:(Times-1)) {
+    limit=min(t1+d,Times)    
+    
+    for (t2 in (t1+1):limit) {
+      B = (1+b)^(abs(outer(c(1,abs(t2-t1+1)),c(1,abs(t2-t1+1)) , "-"))) 
+      cov_matrix=theta2*B
+      N.sample = exp(mvrnorm(nsim, mu = mu, Sigma = cov_matrix))
+      logval = colSums(dpois(Nstar[c(t1,t2)],t(N.sample),log=TRUE))
+      curr = curr-log(nsim) + log( sum(exp(logval)))
+    }    
+  
+  }
+ 
+  return(curr)
+}
+################################################################################
+
+
+
+################################################################################
+log_targetPoissonGauss = function(x,n,tauSq,mu){
+  -exp(x) + x*n - 0.5/tauSq * (x-mu)^2
+}
+################################################################################
+
+
+
+################################################################################
+log_proposalGauss = function(x,tauSq,xi){
+  - 0.5/tauSq * (x-xi)^2
+}
+################################################################################
+
+
+
+
+
